e73c9d4c56d61a6db97d15cc8a9d9bf84d669872
Axel Zaidanaufal <axelzaidanaufalnutika06@gmail.com>
2025-12-29 19:43:36 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -76,7 +76,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="card-body">
             <div class="text-center mb-3">
                 <i class="bi bi-person-circle h1 display-4"></i>
-                <p>Car Lexicon</p>
+                <p>Welcome to My Car Lexicon</p>
                 <hr />
             </div>
             <form action="" method="post">
